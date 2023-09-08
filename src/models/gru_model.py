cf701e8bcfe928f9a222372619119b89f3a3c4d7
cabeywic <charaka@basilurtea.com>
2023-09-08 09:05:15 +0530

    Updated GRU

--- src/models/gru_model.py
+++ src/models/gru_model.py
@@ -3,16 +3,17 @@ import torch
 
 
 class GRUModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim, dropout_prob=0.5):
         super(GRUModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
-        
-        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, dropout=dropout_prob, batch_first=True)
+        self.dropout = nn.Dropout(dropout_prob)
         self.linear = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device).requires_grad_()
-        out, _ = self.gru(x, h0.detach())
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
+        out, _ = self.gru(x, h0)
+        out = self.dropout(out)
         out = self.linear(out[:, -1, :])
         return out
